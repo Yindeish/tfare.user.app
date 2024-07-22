ce942dd1b96f47590bae24d854b66e6661fc172c
Adam Adeshina <adeshinaadam03@gmail.com>
2024-07-22 11:05:56 +0100

    configured local & deployed api url

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,5 +1,9 @@
+
+const NODE_ENV: 'development' | 'production' = process.env.NODE_ENV as 'development' | 'production';
+const DEPLOYED_API_URL = process.env.DEPLOYED_API_URL;
 const IP_ADDRESS = '172.20.10.4';
-const baseUrl = `http://${IP_ADDRESS}:3000/api/v0`;
+const LOCAL_AP_URL = `http://${IP_ADDRESS}:3000/api/v0`;
+const baseUrl = NODE_ENV === 'development' ? LOCAL_AP_URL : DEPLOYED_API_URL;
 
 const URLS = {
     baseUrl,
